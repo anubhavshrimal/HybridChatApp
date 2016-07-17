7257cb134a26f95820c969e72de4f8157ee17a4e
anubhavshrimal <anubhavshrimal@gmail.com>
2016-07-18 01:37:53 +0530

    added verification through firebase OTP feature

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -16,7 +16,7 @@ angular.module('app.controllers', [])
 
 })
 
-.controller('signUpCtrl', function($scope, dialCodes, currentLocationDetails, $localForage, users, setNumber) {
+.controller('signUpCtrl', function($scope, dialCodes, currentLocationDetails, $localForage, OTP, setNumber) {
   $scope.dialList = dialCodes; // get the list of countries to populate the drop-down.
 
   $scope.country = dialCodes[97]; // by default india is selected.
@@ -32,33 +32,63 @@ angular.module('app.controllers', [])
     m = $scope.country.dial_code+m;
     $localForage.setItem("mobileNum", m);
     setNumber.number = m;
-    var obj = users.get(m);
-
-    var OTP = "";
-    for(var i = 0; i < 5; i++){     // generating random OTP
-      OTP += Math.floor(Math.random()*9)+1;
-    }
-    var temp = {"OTP": {"password":OTP, "timestamp": new Date().getTime()}};
-    for(attr in temp){
-      obj[attr] = temp[attr];
-    }
-    obj.$save();
+    OTP.generate(m);
   }
 })
 
-.controller('verificationCtrl', function($scope, getNumber) {
-  console.log(getNumber.number);
+.controller('verificationCtrl', function($scope, getNumber, $ionicPopup, otpService, $state, OTP) {
+  var otpObj = otpService.get(getNumber.number);
+
+  $scope.verify = function (otp) {
+    var date = new Date().getTime();
+
+    // when user data is loaded
+    otpObj.$loaded().then(function () {
+
+      // if OTP is entered in 10 minutes of generation
+      if(parseInt(date) - parseInt(otpObj.timestamp) <= 600000){
+
+        // if otp matches redirect to welcome page else show error
+        if(otpObj.password == otp){
+          otpObj.$remove().then(function () {
+            $state.go('welcome');
+          });
+        }
+        else{
+          $ionicPopup.alert({
+            title: 'Incorrect OTP',
+            template: 'please check the OTP again.'
+          });
+        }
+      }
+      else{ // generate a new OTP if the previous expires
+        OTP.generate(getNumber.number);
+        $ionicPopup.alert({
+          title: 'OTP Expired',
+          template: 'Generating new OTP.'
+        });
+      }
+    });
+  }
+
+  // generate a new OTP request
+  $scope.resendOTP = function () {
+    OTP.generate(getNumber.number);
+    $ionicPopup.alert({
+      title: 'OTP Generated',
+      template: 'Please check your inbox again.'
+    });
+  }
 })
 
-.controller('welcomeCtrl', function($scope) {
-
+.controller('welcomeCtrl', function($scope, $ionicHistory) {
+  $ionicHistory.clearHistory();
 })
 
-.controller('privateChatCtrl', function($scope, $ionicHistory) {
+.controller('privateChatCtrl', function($scope, goBack) {
   $scope.name ="abc";
-  $scope.myGoBack = function () {
-    $ionicHistory.goBack();
-  }
+  $scope.myGoBack = goBack;
+
 })
 
 .controller('chatBotCtrl', function($scope) {
